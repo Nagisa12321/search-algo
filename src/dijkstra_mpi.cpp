99f954bd8c9d4fdb29fa3f8e0b41194fad3e760a
jt <cjt807916@gmail.com>
2021-12-25 14:52:33 +0800

    feat: half compliete dijkstra MPI

--- src/dijkstra_mpi.cpp
+++ src/dijkstra_mpi.cpp
@@ -31,20 +31,25 @@ int main(int argc, char **argv) {
   MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-  // 
-  // Open File 
+  //
+  // Open File
   //
   const char *file_connected = "./connected.txt";
   const char *file_dist_to = "./dist_to.txt";
+  const char *file_variables = "./dist_to.txt";
   // Create two files.
   // connected.txt and dist_to.txt
-  MPI_File fh_connected, fh_dist_to;
-  
+  MPI_File fh_connected, fh_dist_to, fh_variables;
+
   MPI_File_open(MPI_COMM_WORLD, file_connected,
-          MPI_MODE_RDWR /*| MPI_MODE_DELETE_ON_CLOSE */| MPI_MODE_CREATE, MPI_INFO_NULL,
-          &fh_connected);
-  MPI_File_open(MPI_COMM_WORLD, file_dist_to, MPI_MODE_RDWR |/* MPI_MODE_DELETE_ON_CLOSE | */MPI_MODE_CREATE,
+                MPI_MODE_RDWR | MPI_MODE_DELETE_ON_CLOSE | MPI_MODE_CREATE,
+                MPI_INFO_NULL, &fh_connected);
+  MPI_File_open(MPI_COMM_WORLD, file_dist_to,
+                MPI_MODE_RDWR | MPI_MODE_DELETE_ON_CLOSE | MPI_MODE_CREATE,
                 MPI_INFO_NULL, &fh_dist_to);
+  MPI_File_open(MPI_COMM_WORLD, file_variables,
+                MPI_MODE_RDWR | MPI_MODE_DELETE_ON_CLOSE | MPI_MODE_CREATE,
+                MPI_INFO_NULL, &fh_variables);
 
   if (my_rank != 0) {
     int nv;
@@ -94,36 +99,93 @@ int main(int argc, char **argv) {
     });
 
     //
-    // Now start the dijkstra algo
-    //
-    // Open the file every process.
-    //
-    // MPI_File_open(MPI_COMM_WORLD, file_connected,
-    //               MPI_MODE_RDWR | MPI_MODE_CREATE, MPI_INFO_NULL,
-    //               &fh_connected);
-    // MPI_File_open(MPI_COMM_WORLD, file_dist_to, MPI_MODE_RDWR | MPI_MODE_CREATE,
-    //               MPI_INFO_NULL, &fh_dist_to);
-
-
-    if (my_rank == 1) {
-      int *connected = new int[nv];
-      double *dist_to = new double[nv];
-      memset(connected, 0, nv * sizeof(int));
-      memset(dist_to, 0, nv * sizeof(double));
-
-      // Write the file
-      MPI_File_write(fh_connected, connected, nv, MPI_INT, MPI_STATUS_IGNORE);
-      MPI_File_write(fh_dist_to, dist_to, nv, MPI_DOUBLE, MPI_STATUS_IGNORE);
-
-      delete[] connected;
-      delete[] dist_to;
-    }
-
-    MPI_File_close(&fh_connected);
-    //
     // Start the real algo
     //
+    double *dist_to = new double[nv];
+    int *connected = new int[nv];
+    int mv;
+    double md;
 
+    for (int my_step = 1; my_step < nv; ++my_step) {
+      // Init the md and mv. 
+      if (my_rank == 1) {
+        md = inf;
+        mv = -1;
+        MPI_File_write_at(fh_variables, 0, &md, 1, MPI_DOUBLE, MPI_STATUS_IGNORE);
+        MPI_File_write_at(fh_variables, 1, &mv, 1, MPI_INT, MPI_STATUS_IGNORE);
+      }
+      
+      double my_md = inf;
+      int my_mv = -1;
+      //
+      //  Each process finds the nearest unconnected node in its part of the graph.
+      //  Some threads might have no unconnected nodes left.
+      //  get dist_to and connected. 
+      //
+      MPI_File_read(fh_connected, connected, nv, MPI_INT, MPI_STATUS_IGNORE);
+      MPI_File_read(fh_dist_to, dist_to, nv, MPI_DOUBLE, MPI_STATUS_IGNORE);
+      for (int i = my_first; i <= my_last; ++i) {
+        if (!connected[i] && dist_to[i] < my_md) {
+          my_md = dist_to[i];
+          my_mv = i;
+        }
+      }
+
+      //
+      // Choose the best mv, md
+      //
+      cycle_criticalregion(comm_sz, my_rank, [&]{
+        //
+        // Read the mv and md. 
+        //
+        MPI_File_read_at(fh_variables, 0, &mv, 1, MPI_INT, MPI_STATUS_IGNORE);
+        MPI_File_read_at(fh_variables, 1, &md, 1, MPI_DOUBLE, MPI_STATUS_IGNORE);
+
+        // 
+        // If I have greate mv and md 
+        // renew it!
+        // 
+        if (my_md < md) {
+          MPI_File_write_at(fh_variables, 0, &md, 1, MPI_DOUBLE, MPI_STATUS_IGNORE);
+          MPI_File_write_at(fh_variables, 1, &mv, 1, MPI_INT, MPI_STATUS_IGNORE);
+        }
+      });
+
+      //
+      // make sure that md is the small path to a new node.
+      //
+      MPI_Barrier(MPI_COMM_WORLD);
+      //
+      // Read the mv and md. 
+      // mv and md are now the latest and correct values
+      //
+      MPI_File_read_at(fh_variables, 0, &mv, 1, MPI_INT, MPI_STATUS_IGNORE);
+      MPI_File_read_at(fh_variables, 1, &md, 1, MPI_DOUBLE, MPI_STATUS_IGNORE);
+      if (mv != -1) {
+        for (int i = my_first; i <= my_last; ++i) {
+          if (!connected[i]) {
+            //
+            // Why we can do this ? 
+            // Because mv is now in the tree. 
+            // So dist_to[mv] is the correct value!
+            // 
+            if (dist_to[i] > dist_to[mv] + distance(my_graph, mv, i)) {
+              dist_to[i] = dist_to[mv] + distance(my_graph, mv, i);
+            }
+          }
+        }
+      }
+
+      // update the dist_to to the file . 
+
+      //
+      // Make sure that every thread have update the dist_to.
+      // 
+      MPI_Barrier(MPI_COMM_WORLD);
+    }
+
+    delete[] connected;
+    delete[] dist_to;
   } else {
     //
     // Read the input file
@@ -190,8 +252,7 @@ int main(int argc, char **argv) {
     for (int rank = 1; rank < comm_sz; ++rank) {
       int first = ((rank - 1) * nv) / (comm_sz - 1);
       int last = ((rank)*nv) / (comm_sz - 1) - 1;
-      cout << "index " << first << " to " << last << " will send to process "
-           << rank << endl;
+			
       for (int i = first; i <= last; ++i) {
         double *tmp = new double[nv];
         for (int v = 0; v < nv; ++v) {
@@ -204,18 +265,41 @@ int main(int argc, char **argv) {
     }
 
     //
+    // Setup the connected and dist_to. 
+    //
+    int *connected = new int[nv];
+    double *dist_to = new double[nv];
+    memset(connected, 0, nv * sizeof(int));
+    memset(dist_to, 0, nv * sizeof(double));
+
+    // 
+    // setup the connected and dist_to
+    // 
+    connected[0] = true;
+    for (int i = 0; i < nv; ++i) 
+      dist_to[i] = distance(graph, 0, i);
+    //
+    // Write the file
+    // 1) init the connected/dist_to
+    // 2) init the md, mv
+    // 
+    MPI_File_write(fh_connected, connected, nv, MPI_INT, MPI_STATUS_IGNORE);
+    MPI_File_write(fh_dist_to, dist_to, nv, MPI_DOUBLE, MPI_STATUS_IGNORE);
+
+    delete[] connected;
+    delete[] dist_to;
+
+    //
     // Make a criticalregion
     //
     MPI_Send(&nv, 1, MPI_INT, 1, lock_tag, MPI_COMM_WORLD);
-
   }
 
   //
-  // Close the opened file. 
+  // Close the opened file.
   //
   MPI_File_close(&fh_connected);
   MPI_File_close(&fh_dist_to);
-
 
   MPI_Finalize();
 }
